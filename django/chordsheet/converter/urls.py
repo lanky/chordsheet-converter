5e8245e4104d16f497aeff65daa2455d48550b54
Stuart Sears <stuart@sjsears.com>
2014-11-10 08:16:49 +0000

    added django project skeleton

---
+++ django/chordsheet/converter/urls.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env/python
+# -*- coding: utf-8 -*-
+# vim: set ts=4 sts=4 sw=4 ft=python cindent:
+# Copyright 2013 Stuart Sears <stuart.sears@man.com>
+#
+# This file is part of spw-api-scripts
+#
+# The Curl downloader element is based upon work by Chris Oliver, found here:
+# http://excid3.com/blog/resume-downloads-with-pycurl-resume_from.html
+# and adapted slightly to handle SSL with satellite (no verification etc)
+# consequently the license version here has been bumped to 3 to tally with his work.
+#
+# spw-api-scripts is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option)
+# any later version.
+#
+# spw-api-scripts is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with spw-api-scripts. If not, see http://www.gnu.org/licenses/.
+
+"""
+URL mappings for the chordsheet conversion app
+"""
+# from 
